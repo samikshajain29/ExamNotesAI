682b6be7cf32aa9f14a48c858c4342d40c56fa74
Samiksha Jain <samikshaj7371@gmail.com>
2026-03-06 15:44:57 +0530

    create generate notes api

--- server/controllers/generateController.js
+++ server/controllers/generateController.js
@@ -1,4 +1,73 @@
+import Notes from "../models/notes.model.js";
+import User from "../models/user.model.js";
+import { generateGeminiResponse } from "../services/gemini.services.js";
+import { buildPrompt } from "../utils/promptBuilder.js";
+
 export const generateNotes = async (req, res) => {
   try {
-  } catch (error) {}
+    const {
+      topic,
+      classLevel,
+      examType,
+      revisionMode = false,
+      includeDiagram = false,
+      includeChart = false,
+    } = req.body;
+    if (!topic) {
+      return res.status(400).json({ message: "Topic is required" });
+    }
+    const user = await User.findOne(req.userId);
+    if (!user) {
+      return res.status(400).json({ message: "User is not found" });
+    }
+    if (user.credits < 10) {
+      user.isCreditAvailable = false;
+      await user.save();
+      return res.status(400).json({ message: "Insufficient credits" });
+    }
+
+    const prompt = buildPrompt({
+      topic,
+      classLevel,
+      examType,
+      revisionMode,
+      includeDiagram,
+      includeChart,
+    });
+
+    const aiResponse = generateGeminiResponse(prompt);
+
+    const notes = await Notes.create({
+      user: user._id,
+      topic,
+      classLevel,
+      examType,
+      revisionMode,
+      includeDiagram,
+      includeChart,
+      content: aiResponse,
+    });
+
+    user.credits -= 10;
+    if (user.credits <= 0) user.isCreditAvailable = false;
+
+    if (!Array.isArray(user.notes)) {
+      user.notes = [];
+    }
+
+    user.notes.push(notes._id);
+    await user.save();
+
+    return res.status(200).json({
+      data: aiResponse,
+      noteId: note._id,
+      creditsLeft: user.credits,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "AI generation failed",
+      message: error.message,
+    });
+  }
 };
--- server/models/notes.model.js
+++ server/models/notes.model.js
@@ -1,3 +1,32 @@
 import mongoose from "mongoose";
 
-const notesSchema = new mongoose.Schema({ timestamps: true });
+const notesSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    topic: {
+      type: String,
+      required: true,
+    },
+    classLevel: String,
+    examType: Sttring,
+    revisionMode: {
+      type: Boolean,
+      default: false,
+    },
+    includeDiagram: Boolean,
+    includeChart: Boolean,
+    content: {
+      type: mongoose.Schema.Types.Mixed, // AI response (string/JSON)
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Notes = mongoose.model("Notes", notesSchema);
+
+export default Notes;
