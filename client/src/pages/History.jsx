d6085f9d5cdc932f41b0af9d5920be3b14c5812c
Samiksha Jain <samikshaj7371@gmail.com>
2026-03-29 00:21:37 +0530

    Start making history page

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -1,7 +1,76 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { serverUrl } from "../App";
+import { AnimatePresence, motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function History() {
-  return <div></div>;
+  const navigate = useNavigate();
+  const { userData } = useSelector((state) => state.user);
+  const credits = userData.credits;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [topics, setTopics] = useState([]);
+  useEffect(() => {
+    const myNotes = async () => {
+      try {
+        const res = await axios.get(serverUrl + "/api/notes/getNotes", {
+          withCredentials: true,
+        });
+        console.log(res.data);
+        setTopics(Array.isArray(res.data) ? res.data : []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    myNotes();
+  }, []);
+  return (
+    <div className="min-h-screen bg-linear-to-br from-gray-100 to-gray-200 px-6 py-8">
+      <motion.header
+        initial={{ opacity: 0, y: -15 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="mb-10 rounded-2xl bg-black/80 backdrop-blur-xl border border-white/10 px-8 py-6 items-start flex justify-between md:items-center gap-4 flex-wrap shadow-[0_20px_45px_rgba(0,0,0,0.6)]"
+      >
+        <div onClick={() => navigate("/")} className="cursor-pointer">
+          <h1 className="text-2xl font-bold bg-linear-to-r from-white via-gray-300 to-white bg-clip-text text-transparent">
+            ExamNotes AI
+          </h1>
+          <p className="text-sm text-gray-300 mt-1">
+            AI powered exam-oriented notes & revision
+          </p>
+        </div>
+
+        <div className="flex items-center gap-4 ">
+          <button className="lg:hidden text-white text-2xl">
+            <GiHamburgerMenu />
+          </button>
+          <button
+            className="flex items-center gap-2 px-4 py-2 rounded-full bg-white/10 border border-white/20 text-white text-sm"
+            onClick={() => navigate("/pricing")}
+          >
+            <span className="text-lg">🔷</span>
+            <span>{credits}</span>
+
+            <motion.span
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.97 }}
+              className="ml-2 h-5 w-5 flex items-center justify-center rounded-full bg-white text-xs font-bold"
+            >
+              ➕
+            </motion.span>
+          </button>
+        </div>
+      </motion.header>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+        <AnimatePresence></AnimatePresence>
+      </div>
+    </div>
+  );
 }
 
 export default History;
--- server/routes/generateRoutes.js
+++ server/routes/generateRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
 import { generateNotes } from "../controllers/generateController.js";
+import { getMyNotes, getSingleNotes } from "../controllers/notesController.js";
 
 const notesRouter = express.Router();
 
 notesRouter.post("/generate-notes", isAuth, generateNotes);
+notesRouter.get("/getnotes", isAuth, getMyNotes);
+notesRouter.get("/:id", isAuth, getSingleNotes);
 
 export default notesRouter;
