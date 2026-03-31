c519f353b3338c12b516d43e868066c535ec6854
Samiksha Jain <samikshaj7371@gmail.com>
2026-04-01 00:03:24 +0530

    new changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,8 +14,8 @@ export const googleAuth = async (req, res) => {
     let token = await getToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      samesite: "strict",
+      secure: true,
+      samesite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
