0a0cdc563a953e7c4ec0cb780aa2ed2ea5e2ea00
Samiksha Jain <samikshaj7371@gmail.com>
2026-04-01 18:15:23 +0530

    Fix typo in cookie options for authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,7 +15,8 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      samesite: "none",
+      sameSite: "none",
+      path:"/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
