5314603a702e4cf8543c0f8a1f90385a0cb7a4aa
Samiksha Jain <samikshaj7371@gmail.com>
2026-04-01 18:09:22 +0530

    Change CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://examnotesaiclient-zqxj.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
